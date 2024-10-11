31c38272c54cb19cfc7bff4a0ad7f8d7e8a65b0e
saravan.prathi <saravan.prathi@gmail.com>
2024-10-11 22:48:34 +0100

    Investment calculator built with React

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,58 @@
+
+
+export default function UserInput({userInput, handleChange}) {
+  
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            required
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            required
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            value={userInput.duration}
+            required
+            onChange={(event) =>
+              handleChange("duration", event.target.value)
+            }
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
